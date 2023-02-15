b4e625e6e8a58541e12e7acebedf7b9431d96612
barkatzx <barkatullah.zx@gmail.com>
2023-02-16 00:43:16 +0600

    Nothing

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    screens: {
+      xs: "480px",
+      sm: "576px",
+      md: "960px",
+      lg: "1440px",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
